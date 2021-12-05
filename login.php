8b1fe46389224e66ff30235cc991c476241747ba
Edward McDonald <70046857+NotMarkZuckerburg@users.noreply.github.com>
2021-12-05 02:54:08 -0800

    minor

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
     $conn = new mysqli('localhost', 'user', 'password', 'main');
 
-        $USERNAME = $_GET['username'];
-        $PASSWORD = $_GET['password'];
+        $USERNAME = $_GET["username"];
+        $PASSWORD = $_GET["password"];
 
         $sql = "INSERT INTO loginInfo (username, pass_word)
                 VALUES ($USERNAME, $PASSWORD);";
