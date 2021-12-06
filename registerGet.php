63dabe12a247ae8a5b357b722277fbae29f4ab07
Edward McDonald <70046857+NotMarkZuckerburg@users.noreply.github.com>
2021-12-05 18:32:30 -0800

    m

--- registerGet.php
+++ registerGet.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('localhost', 'user', 'password', 'main');
+    //$conn = new mysqli('localhost', 'user', 'password', 'main');
 
         $FIRSTNAME = $_GET['FirstName'];
         $LASTNAME = $_GET['LastName'];
@@ -10,7 +10,12 @@
         $USERNAME = $_GET['username'];
         $PASSWORD = $_GET['Password'];
 
-        $sql = "INSERT INTO userinfo (first_name, last_name, username, email, pass_word)
+        $sql = "SELECT username
+                FROM userInfo
+                WHERE EXISTS (SELECT username FROM userInfo WHERE username = '$USERNAME');";
+        $conn->query($sql);
+
+        $sql = "INSERT INTO userInfo (first_name, last_name, username, email, pass_word)
                 VALUES ('$FIRSTNAME', '$LASTNAME', '$USERNAME', '$EMAIL', '$PASSWORD');";
 
                 if ($conn->query($sql) === TRUE) {
